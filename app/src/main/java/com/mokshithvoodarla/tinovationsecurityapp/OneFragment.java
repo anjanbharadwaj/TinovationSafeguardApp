15c4b4970eda93a7f833813ff167232ea90e1138
hydefbla <anjanbharadwaj02@gmail.com>
2017-05-27 11:01:13 -0700

    Added all the descriptions from Firebase (the text stream that will be paired with the image)

--- app/src/main/java/com/mokshithvoodarla/tinovationsecurityapp/OneFragment.java
+++ app/src/main/java/com/mokshithvoodarla/tinovationsecurityapp/OneFragment.java
@@ -13,10 +13,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class OneFragment extends Fragment{
+    List<ProfileInfo> result;
+    String username;
+    String time;
+    String info1 = "";
+    DatabaseReference root;
+    ArrayList<String> info;
+    ProfileInfo ci;
 
     public OneFragment() {
         // Required empty public constructor
@@ -38,27 +52,61 @@ public class OneFragment extends Fragment{
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        RecyclerView recList = (RecyclerView) getView().findViewById(R.id.cardList);
+        username = getActivity().getIntent().getExtras().get("Username").toString();
+        root = FirebaseDatabase.getInstance().getReference().child(username).child("Stream");
+
+        final RecyclerView recList = (RecyclerView) getView().findViewById(R.id.cardList);
         recList.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(getContext());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         recList.setLayoutManager(llm);
-        ProfileAdapter adapter = new ProfileAdapter(createList(21));
-        recList.setAdapter(adapter);
-    }
-    private List<ProfileInfo> createList(int size) {
+        root.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("An event was added!");
+                ArrayList<String> data = new ArrayList<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+                while(i.hasNext()){
+                    data.add(((DataSnapshot)i.next()).getKey());
+                }
 
-        List<ProfileInfo> result = new ArrayList<>();
-        for (int i=1; i <= size; i++) {
-            ProfileInfo ci = new ProfileInfo();
-            ci.name="akimeme"+i;
-            ci.description="akiisdank"+i;
-            ci.action1="eataki"+i;
-            ci.action2="eataki"+i;
-            result.add(ci);
+                ProfileAdapter adapter = new ProfileAdapter(createList(data));
+                recList.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+    private List<ProfileInfo> createList(final ArrayList<String> times) {
+        result = new ArrayList<>();
+        for (int j=0; j < times.size(); j++) {
+            time = times.get(j);
+            doIt(time);
 
         }
 
         return result;
+    }
+    public void doIt(final String time){
+        System.out.println("I just entered doIt");
+        root.child(time).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                info1 = dataSnapshot.child("event").getValue().toString();
+                ci = new ProfileInfo();
+                ci.name="New visitor for " + username;
+                ci.description="Time:" + time + "," + info1;
+                result.add(ci);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
